1f266e9cf6a7d8ea431d040c59b4606993bb75ea
unknown <525224259@qq.com>
2026-07-03 14:17:51 +0800

    修改

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = '我的博客';
-export const SITE_DESCRIPTION = '一个关于技术、生活和摄影的个人博客';
+export const SITE_TITLE = '旺仔的博客';
+export const SITE_DESCRIPTION = '10年前端工程师转型AI Agent开发，记录技术探索、学习笔记和生活感悟';
